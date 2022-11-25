2ca0625dd484358f67ee07708bc27889f3b94994
andreresende36 <andreresende36@gmail.com>
2022-11-25 19:53:48 -0300

    Resolução do requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+
+export default SolarSystem;
